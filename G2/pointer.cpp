93639f7a107cefed3025e00ef71609af7f7753ee
DuramAsh <duramash.02@gmail.com>
2022-09-06 20:31:09 +0600

    init

---
+++ G2/pointer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+bool search(int ar[], int target) {
+
+}
+
+bool search2(int &ar, int target) {
+
+}
+
+int main() {
+
+	cout << search({1, 2, 3, ..., 1e9}, 66);
+
+	return 0;
+}
+
+(n - 1)!
+--------
+	2
---
+++ G2/two_sum_two.cpp
@@ -0,0 +1,22 @@
+vector<int> twoSum(vector<int> &nums, int target)
+{
+	map<int, int> m;
+	vector<int> answer(2);
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		if (m.find(target - nums[i]) != m.end())
+		{
+			answer[0] = m[target - nums[i]];
+			answer[1] = i;
+			break;
+		}
+		else
+		{
+			m[nums[i]] = i;
+		}
+	}
+	return answer;
+}
+
+// Time complexity: O(n)
+// Space complexity: O(n + m)
