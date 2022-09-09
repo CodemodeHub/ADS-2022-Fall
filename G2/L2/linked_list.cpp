b3e724ab3817df2abfd82e0be2cc7faa9b482f7f
DuramAsh <duramash.02@gmail.com>
2022-09-09 20:19:54 +0600

    l2

--- G2/L2/linked_list.cpp
+++ G2/L2/linked_list.cpp
@@ -31,48 +31,65 @@ struct LinkedList
 	ListNode *head;
 	ListNode *tail;
 
-	LinkedList() {
+	LinkedList()
+	{
 		this->size = 0;
 		this->head = nullptr;
 		this->tail = nullptr;
 	}
 
-	void addAtHead(int val) {
-		this->size++;
-		ListNode *temp = new ListNode(val);
-		if (this->head == nullptr) {
-			this->head = temp;
-			this->tail = temp;
-		} else {
-			temp->next = this->head;
-			this->head = temp;
-		}
-	}
-
-	void addAtTail(int val) {
+	void addAtHead(int val)
+	{
 		this->size++;
 		ListNode *temp = new ListNode(val);
 		if (this->head == nullptr)
 		{
 			this->head = temp;
 			this->tail = temp;
-		} else {
+		}
+		else
+		{
+			temp->next = this->head;
+			this->head = temp;
+		}
+	}
+
+	void addAtTail(int val)
+	{
+		this->size++;
+		ListNode *temp = new ListNode(val);
+		if (this->head == nullptr)
+		{
+			this->head = temp;
+			this->tail = temp;
+		}
+		else
+		{
 			this->tail->next = temp;
 			this->tail = temp;
 		}
 	}
 
-	void addAtIndex(int index, int val) {
-		if (index > this->size) {
+	void addAtIndex(int index, int val)
+	{
+		if (index > this->size)
+		{
 			return;
-		} else if (index == 0) {
+		}
+		else if (index == 0)
+		{
 			this->addAtHead(val);
-		} else if (index == this->size) {
+		}
+		else if (index == this->size)
+		{
 			this->addAtTail(val);
-		} else {
+		}
+		else
+		{
 			ListNode *temp = new ListNode(val);
 			ListNode *cur = this->head;
-			for (int i = 1; i < index; i++) {
+			for (int i = 1; i < index; i++)
+			{
 				cur = cur->next;
 			}
 			temp->next = cur->next;
@@ -81,22 +98,61 @@ struct LinkedList
 		}
 	}
 
-	int get(int index) {
-		if (index < 0 || index >= this->size) {
+	int get(int index)
+	{
+		if (index < 0 || index >= this->size)
+		{
 			return -1;
 		}
 		ListNode *cur = this->head;
-		for (int i = 0; i < index; i++) {
+		for (int i = 0; i < index; i++)
+		{
 			cur = cur->next;
 		}
 		return cur->val;
 	}
 
+	void deleteAtIndex(int index)
+	{
+		if (index == 0)
+		{
+			this->head = this->head->next;
+			this->size--;
+		}
+		else if (index == this->size - 1)
+		{
+			ListNode *cur = this->head;
+			while (cur->next != this->tail)
+			{
+				cur = cur->next;
+			}
+			this->tail = cur;
+			this->size--;
+		} else if (index > 0 && index < this->size) {
+			ListNode *cur = this->head;
+			for (int i = 0; i < index - 1; i++) {
+				cur = cur->next;
+			}
+			cur->next = cur->next->next;
+			this->size--;
+		}
+	}
+
+	void printLL() {
+		ListNode *cur = this->head;
+		while (cur->next != nullptr) {
+			cout << cur->val << " ";
+			cur = cur->next;
+		}
+		// while (cur->next) {
+
+		// }
+	}
 };
 
 int main()
 {
-	LinkedList* ll = new LinkedList();
+	LinkedList *ll = new LinkedList();
 	// cout << ll->size << endl;
 	ll->addAtHead(10);
 	ll->addAtHead(40);
