2ed7f5401f588f3e3f04e0f0b678ba7657bea808
DuramAsh <duramash.02@gmail.com>
2022-09-10 17:09:49 +0600

    123

--- G3/L2/node.cpp
+++ G3/L2/node.cpp
@@ -6,12 +6,25 @@ struct ListNode {
 	ListNode *next;
 
 	ListNode() {
-		
+		this->val = 0;
+		this->next = nullptr; // NULL, if this doesnt work
+	}
+
+	ListNode(int val) {
+		this->val = val;
+		this->next = nullptr;
+	}
+
+	ListNode(int val, ListNode *next) {
+		this->val = val;
+		this->next = next;
 	}
 };
 
 int main()
 {
-
+	ListNode *myNode1 = new ListNode();
+	ListNode *myNode2 = new ListNode(5, myNode1);
+	cout << myNode1->val;
 	return 0;
 }
