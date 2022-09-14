536f7efaa6ea0fa25acf9c99ca21dfcea8983e63
DuramAsh <duramash.02@gmail.com>
2022-09-14 20:26:43 +0600

    123

---
+++ G3/L3/Leetcode/browser_history.cpp
@@ -0,0 +1,74 @@
+class Node
+{
+public:
+	Node *prev, *next;
+	string val;
+	Node(string val)
+	{
+		this->val = val;
+		this->prev = nullptr;
+		this->next = nullptr;
+	}
+};
+
+class BrowserHistory
+{
+private:
+	Node *cur;
+
+public:
+	BrowserHistory(string homepage)
+	{
+		this->cur = new Node(homepage);
+	}
+
+	void visit(string url)
+	{
+		Node *newPage = new Node(url);
+		this->cur->next = newPage;
+		newPage->prev = this->cur;
+		this->cur = newPage;
+		// this->cur = this->cur->next;
+	}
+
+	string back(int steps)
+	{
+		while (steps--)
+		{
+			if (this->cur->prev != nullptr)
+			{
+				this->cur = this->cur->prev;
+			}
+			else
+			{
+				break;
+			}
+		}
+		return this->cur->val;
+	}
+
+	string forward(int steps)
+	{
+		// for (int i = 0; i < steps; i++)
+		while (steps--)
+		{
+			if (this->cur->next != nullptr)
+			{
+				this->cur = this->cur->next;
+			}
+			else
+			{
+				break;
+			}
+		}
+		return this->cur->val;
+	}
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
---
+++ G3/L3/aib.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	string s;
+	Node *next;
+
+	Node(string s)
+	{
+		this->s = s;
+		next = nullptr;
+	}
+};
+struct LinkedList
+{
+	Node *head;
+	Node *tail;
+	int size;
+	int index;
+
+	LinkedList()
+	{
+		head = nullptr;
+		tail = nullptr;
+		size = 0;
+		index = 0;
+	}
+
+	void push(string s)
+	{
+		Node *newNode = new Node(s);
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	void print()
+	{
+		Node *cur = head;
+
+		while (cur != nullptr)
+		{
+			cout << cur->s << " ";
+			cur = cur->next;
+		}
+	}
+
+	void change(int k)
+	{
+		Node *cur = this->head;
+		cout << head->s;
+		while (k--)
+		{
+			this->head = this->head->next;
+			cur->next = nullptr;
+			this->tail->next = cur;
+			this->tail = cur;
+		}
+	}
+};
+
+int main()
+{
+	LinkedList list;
+	int n, k;
+	cin >> n >> k;
+	string s;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> s;
+		list.push(s);
+	}
+
+	list.change(k);
+	// list.print();
+}
+// 5
+// 2
+// the show must go on
