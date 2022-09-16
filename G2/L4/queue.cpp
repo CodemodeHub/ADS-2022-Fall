8199f4bf7368b9bfa1f9417ed28fb792952538fb
DuramAsh <duramash.02@gmail.com>
2022-09-16 20:01:40 +0600

    123

--- G2/L4/queue.cpp
+++ G2/L4/queue.cpp
@@ -24,25 +24,58 @@ struct Queue {
 	}
 
 	void pop() {
-
+		if (this->isEmpty()) {
+			throw std::invalid_argument("queue is empty");
+		} else {
+			Node *toDel = beti;
+			beti = beti->next;
+			delete (toDel);
+		}
+		size--;
 	}
 
 	int front() {
-
+		return beti->val;
 	}
 
 	void push(int val) {
-
+		if (this->isEmpty()) {
+			Node *newNode = new Node(val);
+			beti = newNode;
+			koteni = newNode;
+		} else {
+			Node *newNode = new Node(val);
+			koteni->next = newNode;
+			koteni = newNode;
+		}
+		size++;
 	}
 
 	bool isEmpty() {
-
+		return size == 0;
 	}
 
 };
 
 int main()
 {
-
+	Queue q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+	q.push(5);
+	q.pop();
+	q.pop();
+	q.pop();
+	q.pop();
+	q.push(10);
+	q.push(10);
+	q.push(10);
+	while (!q.isEmpty()) {
+		cout << q.front() << " ";
+		q.pop();
+	}
+	cout << endl;
 	return 0;
 }
