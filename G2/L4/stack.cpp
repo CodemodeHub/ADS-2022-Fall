f5bc997e83702ff942ff534be6a518ff1b05ecb2
DuramAsh <duramash.02@gmail.com>
2022-09-16 19:28:03 +0600

    123

--- G2/L4/stack.cpp
+++ G2/L4/stack.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+
 struct Node {
 	int val;
 	Node *next;
@@ -10,7 +11,51 @@ struct Node {
 	}
 };
 
+
+struct Stack {
+	Node *top;
+
+	Stack() {
+		this->top = nullptr;
+	}
+
+	void push(int val) {
+		if (this->isEmpty()) {
+			top = new Node(val);
+		} else {
+			Node* newNode = new Node(val);
+			newNode->next = top;
+			top = newNode;
+		}
+	}
+
+	void pop() {
+		if (this->isEmpty()) {
+			throw std::invalid_argument("the stack is empty!!!");
+		} else {
+			Node *toDelete = top;
+			top = top->next;
+			delete (toDelete);
+		}
+	}
+
+	int top() {
+		return top->val;
+	}
+
+	bool isEmpty() {
+		return top == nullptr;
+	}
+};
+
 int main() {
+	// Stack *st = new Stack();
+	Stack st;
+	int n;
+	cin >> n;
+	while (n--) {
+		
+	}
 
 	return 0;
 }
