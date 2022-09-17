e58a04e208bf361e6b594433f160b569ea9a9fab
DuramAsh <duramash.02@gmail.com>
2022-09-17 18:00:18 +0600

    123

--- G3/L4/queue.cpp
+++ G3/L4/queue.cpp
@@ -18,31 +18,73 @@ struct Queue
 	Node *head;
 	Node *tail;
 
-	Queue() {
+	Queue()
+	{
 		this->size = 0;
 		this->head = nullptr;
 		this->tail = nullptr;
 	}
 
-	bool isEmpty() {
-
+	bool isEmpty()
+	{
+		return size == 0;
 	}
 
-	void push(int val) {
-
+	void push(int val)
+	{
+		Node *newNode = new Node(val);
+		if (this->isEmpty())
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		size++;
 	}
 
-	void pop() {
-
+	void pop()
+	{
+		if (this->isEmpty())
+		{
+			throw std::invalid_argument("error: queue is empty");
+		}
+		else
+		{
+			Node *toDel = head;
+			head = head->next;
+			size--;
+			delete (toDel);
+		}
 	}
 
-	int front() {
-		
+	int front()
+	{
+		if (this->isEmpty())
+		{
+			throw std::invalid_argument("error: queue is empty");
+		}
+		else
+		{
+			return head->val;
+		}
 	}
 };
 
 int main()
 {
+	Queue q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
 
+	while (!q.isEmpty())
+	{
+		cout << q.front() << " ";
+		q.pop();
+	}
 	return 0;
 }
