4302571488ea1abc1484307143508c94eb261a51
DuramAsh <duramash.02@gmail.com>
2022-09-17 17:19:22 +0600

    123

---
+++ G3/L4/stack.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node {
+	int val;
+	Node *next;
+	Node(int val) {
+		this->val = val;
+		this->next = nullptr;
+	}
+};
+
+struct Stack {
+	int size;
+	Node *top;
+
+	void pop() {
+
+	}
+
+	void push(int val) {
+
+	}
+
+	bool isEmpty() {
+		
+	}
+};
+
+int main() {
+
+	return 0;
+}
