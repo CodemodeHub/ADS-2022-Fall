230d178020091ae148918d6e46c066cce6c6f511
DuramAsh <duramash.02@gmail.com>
2022-09-26 19:44:45 +0600

    123

---
+++ G1/lesson6/Lab3/B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	int ar[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> ar[i];
+	}
+	unsigned long long left = 0, right = 1e9, mid, cnt, sum;
+	while (left < right) {
+		mid = left + (right - left) / 2;
+		cnt = 1;
+		sum = 0;
+		
+	}
+
+	return 0;
+}
