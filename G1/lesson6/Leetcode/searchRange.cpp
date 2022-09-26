6d7ace79d4e512026d7c6417d2fb95a9a94f08b7
DuramAsh <duramash.02@gmail.com>
2022-09-26 20:54:05 +0600

    123

---
+++ G1/lesson6/Leetcode/searchRange.cpp
@@ -0,0 +1,37 @@
+// TODO : DODELAT
+
+class Solution
+{
+public:
+	vector<int> searchRange(vector<int> &nums, int target)
+	{
+		int left = 0, right = nums.size() - 1, mid;
+		while (left < right)
+		{
+			mid = left + (right - left) / 2;
+			if (target < nums[mid])
+			{
+				right = mid - 1;
+			}
+			else if (target > nums[mid])
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				int l = mid, r = mid;
+				while (nums[l] == nums[mid] && l >= 0)
+				{
+					l--;
+				}
+				while (nums[r] == nums[mid] && r < nums.size())
+				{
+					r++;
+				}
+				// return {l - 1, r - 1};
+				return {l + 1, r - 1};
+			}
+		}
+		return {-1, -1};
+	}
+};
