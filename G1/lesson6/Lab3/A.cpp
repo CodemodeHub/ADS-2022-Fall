f869e322ae0a09adbdb6e9238e2a20fde9cdbce6
DuramAsh <duramash.02@gmail.com>
2022-09-26 19:13:49 +0600

    123

---
+++ G1/lesson6/Lab3/A.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int binary_search_inc(int ar[], int target, int left, int right) {
+	if (right < left) {
+		return -1;
+	}
+	int mid = left + (right - left) / 2;
+	if (target < ar[mid]) {
+		return binary_search_inc(ar, target, left, mid - 1);
+	} else if (target > ar[mid]) {
+		return binary_search_inc(ar, target, mid + 1, right);
+	} else {
+		return mid;
+	}
+}
+
+int binary_search_dec(int ar[], int target, int left, int right) {
+	if (right < left) {
+		return -1;
+	}
+	int mid = left + (right - left) / 2;
+	if (target < ar[mid]) {
+		return binary_search_inc(ar, target, mid + 1, right);
+	} else if (target > ar[mid]) {
+		return binary_search_inc(ar, target, left, mid - 1);
+	} else {
+		return mid;
+	}
+}
+
+
+int main() {
+
+	int x;
+	cin >> x;
+	int ar[x];
+	for (int i = 0; i < x; i++) {
+		cin >> ar[i];
+	}
+	int m, n;
+	cin >> m >> n;
+	int ar2d[m][n];
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> ar2d[i][j];
+		}
+	}
+
+	for (int k = 0; k < x; k++) {
+
+	}
+
+	return 0;
+}
