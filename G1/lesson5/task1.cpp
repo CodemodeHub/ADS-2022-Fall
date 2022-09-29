fc39b58eda3e91ab356c71e9cbdc594d89a10a8e
Bauyrzhan <qwe123ez@gmail.com>
2022-09-29 20:59:11 +0600

    lesson6

---
+++ G1/lesson5/task1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n, x;
+    cin>>n;
+    int a[n];
+    for(int i = 0; i < n; i++)
+        cin>>a[i];
+    cin>>x; // i -> min a[i] >= x
+    int l = -1, r = n;
+    while(r - l > 1) {
+        int m = l + (r - l) / 2;
+        if(a[m] > x) {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+    cout<<r<<endl;
+    return 0;
+}
