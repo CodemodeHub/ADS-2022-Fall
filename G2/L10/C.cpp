589ad9b1e554374389f6eabda077d0d663250248
DuramAsh <duramash.02@gmail.com>
2022-10-11 20:17:51 +0600

    123

---
+++ G2/L10/C.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct MaxHeap {
+	vector<int> heap;
+
+	int parent(int i) {
+		return (i - 1) / 2;
+	}
+	int left(int i) {
+		return 2 * i + 1;
+	}
+	int right(int i) {
+		return 2 * i + 2;
+	}
+	void insert(int value) {
+		this->heap.push_back(value);
+		int i = this->heap.size() - 1;
+		while (i != 0 && this->heap[parent(i)] < this->heap[i]) {
+			swap(this->heap[parent(i)], this->heap[i]);
+			i = parent(i);
+		}
+	}
+	int extractMax() {
+		if (this->heap.size() == 0) return INT_MAX;
+		if (this->heap.size() == 1) {
+			int root = this->heap[0];
+			this->heap.pop_back();
+			return root;
+		}
+		int root = this->heap[0];
+		this->heap[0] = this->heap[this->heap.size() - 1];
+		this->heap.pop_back();
+		this->heapify(0);
+		return root;
+	}
+	void heapify(int i) {
+		int l = this->left(i);
+		int r = this->right(i);
+		int smallest = i;
+		if (l < this->heap.size() && this->heap[l] > this->heap[smallest])
+			smallest = l;
+		if (r < this->heap.size() && this->heap[r] > this->heap[smallest])
+			smallest = r;
+		if (smallest != i) {
+			swap(this->heap[i], this->heap[smallest]);
+			this->heapify(smallest);
+		}
+	}
+};
+
+int main() {
+	MaxHeap heap;
+	int n, k;
+	cin >> n >> k;
+	while (n--) {
+		int temp;
+		cin >> temp;
+		heap.insert(temp);
+	}
+	int sum = 0;
+	while (k--) {
+		int maxi = heap.extractMax();
+		sum += maxi;
+		heap.insert(maxi - 1);
+	}
+	cout << sum;
+
+	return 0;
+}
