00bce727f23082ca45065e025480446dfd54018c
DuramAsh <duramash.02@gmail.com>
2022-10-15 18:24:17 +0600

    123

---
+++ G3/L11 STRING, HASING, RABIN-KARP/qsort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int partition(int ar[], int left, int right) {
+	int pivot = ar[right];
+	int index = left;
+	for (int i = left; i < right; i++) {
+		if (ar[i] < pivot) {
+			swap(ar[i], ar[index]);
+			index++;
+		}
+	}
+	swap(ar[index], ar[right]);
+	return index;
+}
+
+void quick_sort(int ar[], int left, int right) {
+	if (left < right) {
+		int p = partition(ar, left, right);
+		quick_sort(ar, left, p - 1);
+		quick_sort(ar, p + 1, right);
+	}
+}
+
+int main() {
+	
+	int ar[] = { 8, 4, 2, 13, 5 };
+	int n = sizeof(ar) / sizeof(ar[0]);
+	quick_sort(ar, 0, n - 1);
+	for (auto i : ar) {
+		cout << i << " ";
+	}
+	return 0;
+}
+// O(NLogN)
