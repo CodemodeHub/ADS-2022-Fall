9883d9b2c4edabcd4b61b7c3beb30f5940891701
DuramAsh <duramash.02@gmail.com>
2022-10-28 20:36:49 +0600

    123

--- Lab Solutions NE TROGAT))/LAB6/G.cpp
+++ Lab Solutions NE TROGAT))/LAB6/G.cpp
@@ -1,27 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool comp(pair<string, string>& p1, pair<string, string>& p2) {
+	return p1.first < p2.first;
+}
+
+int partition(vector<pair<string, string> >& v, int left, int right) {
+	pair<string, string> pivot = v[right];
+	int index = left;
+	for (int i = left; i < right; i++) {
+		if (comp(v[i], pivot)) {
+			swap(v[i], v[index]);
+			index++;
+		}
+	}
+	swap(v[index], v[right]);
+	return index;
+}
+
+void quick_sort(vector<pair<string, string> >& v, int left, int right) {
+	if (left < right) {
+		int p = partition(v, left, right);
+		quick_sort(v, left, p - 1);
+		quick_sort(v, p + 1, right);
+	}
+}
+
 int main() {
 	int n;
-	map<string, string> m;
+	vector<pair<string, string> > v;
 	cin >> n;
 	while (n--) {
 		string oldname, newname;
 		bool flag = false;
 		cin >> oldname >> newname;
-		for (auto i : m) {
-			if (oldname == i.second) {
-				m[i.first] = newname;
+		for (int i = 0; i < v.size(); i++) {
+			if (oldname == v[i].second) {
+				v[i].second = newname;
 				flag = true;
 				break;
 			}
 		}
 		if (!flag) {
-			m[oldname] = newname;
+			v.push_back({ oldname, newname });
 		}
 	}
-		cout << "CHECK\n";
-		for (auto i : m) {
-			cout << i.first << " " << i.second << endl;
-		}
+	quick_sort(v, 0, v.size() - 1);
+	cout << v.size() << "\n";
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i].first << " " << v[i].second << endl;
 	}
+}
