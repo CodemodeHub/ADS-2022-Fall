5ed1b321cf29d3e363ae558732ce127307cd87b9
DuramAsh <duramash.02@gmail.com>
2022-10-29 18:11:55 +0600

    123

---
+++ G3/L15 REVISION/danial.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+	if (b == 0) {
+		return a;
+	}
+	return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+	return a * b / gcd(a, b);
+}
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0) {
+			if (gcd(i, n / i) == 1) {
+				cout << i << " " << n / i;
+				return 0;
+			}
+		}
+	}
+	cout << -1;
+	return 0;
+}
