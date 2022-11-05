d038a0ec6e0d5206cc0ccefea8aa956a0c2514fb
DuramAsh <duramash.02@gmail.com>
2022-11-05 18:50:18 +0600

    123

---
+++ G3/L17 TOPSORT CYCLES COLORS/E.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int> > g;
+vector<bool> vis;
+set<int> deleted_nodes;
+
+void dfs(int v) {
+	vis[v] = true;
+	for (int i = 0; i < g[v].size(); i++) {
+		int u = g[v][i];
+		if (!vis[u] && deleted_nodes.find(u) == deleted_nodes.end()) {
+			dfs(u);
+		}
+	}
+}
+
+int main() {
+	int m, n;
+	cin >> m >> n;
+	g.resize(m);
+	vis.resize(m);
+	for (int i = 0; i < n; i++) {
+		int u, v;
+		cin >> u >> v;
+		u--;
+		v--;
+		g[u].push_back(v);
+	}
+	for (int i = 0; i < m; i++) {
+		deleted_nodes.insert(i);
+		int comp = 0;
+		for (int j = i + 1; j < m; j++) {
+			if (!vis[j]) {
+				dfs(j);
+				comp++;
+			}
+		}
+		vis.clear();
+		vis.resize(m);
+		cout << comp << "\n";
+	}
+
+	return 0;
+}
---
+++ G3/L17 TOPSORT CYCLES COLORS/betatester.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int> > g;
+vector<bool> vis;
+vector<int> topsort_v, colors; // 0 unvis, 1 red, 2 blue
+
+bool detectCycle(int v) {
+	colors[v] = 1;
+	for (int i = 0; i < g[v].size(); i++) {
+		int u = g[v][i];
+		if (colors[u] == 1) {
+			return true;
+		}
+		if (colors[u] == 0) {
+			if (detectCycle(u)) return true;
+		}
+	}
+	colors[v] = 2;
+	return false;
+}
+
+void topsort(int v) {
+	vis[v] = true;
+	for (int i = 0; i < g[v].size(); i++) {
+		int u = g[v][i];
+		if (!vis[u]) {
+			topsort(u);
+		}
+	}
+	topsort_v.push_back(v);
+}
+
+int main() {
+	int m, n;
+	cin >> m >> n;
+	colors.resize(m);
+	g.resize(m);
+	vis.resize(m);
+	for (int i = 0; i < n; i++) {
+		int u, v;
+		cin >> u >> v;
+		u--;
+		v--;
+		g[u].push_back(v);
+	}
+	bool hasCycle = false;
+	for (int i = 0; i < m; i++) {
+		if (detectCycle(i)) {
+			hasCycle = true;
+			break;
+		}
+	}
+	if (!hasCycle) {
+		cout << "Possible\n";
+		for (int i = 0; i < m; i++) {
+			if (!vis[i]) {
+				topsort(i);
+			}
+		}
+		reverse(topsort_v.begin(), topsort_v.end());
+		for (auto i : topsort_v) {
+			cout << i + 1 << " ";
+		}
+	} else {
+		cout << "Impossible";
+	}
+
+	return 0;
+}
