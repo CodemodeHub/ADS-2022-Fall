5e3fe3553424a97f13047af4c85b357615f604f5
DuramAsh <duramash.02@gmail.com>
2022-11-15 20:42:50 +0600

    123

--- G2/L19 KMP PREFIX REVISION/H.cpp
+++ G2/L19 KMP PREFIX REVISION/H.cpp
@@ -1,42 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> prefixFunction(string s) {
-	vector<int> pi(s.length());
-	for (int i = 1; i < s.length(); i++) {
-		int j = pi[i - 1];
-		while (j > 0 && s[i] != s[j]) j = pi[j - 1];
-		if (s[i] == s[j]) j++;
-		pi[i] = j;
-	}
-	return pi;
-}
-
-vector<int> KMP(string txt, string pat) {
-	vector<int> ans;
-	string concat = pat + '#' + txt;
-	vector<int> pi = prefixFunction(concat);
-	for (int i = pat.size() + 1; i < concat.size(); i++) {
-		if (pi[i] == pat.size()) {
-			ans.push_back(i - 2 * pat.size());
-		}
-	}
-	return ans;
-}
-
 int main() {
 	string s;
 	cin >> s;
 	int cnt = 0;
-	for (int i = 1; i < s.size(); i++) {
-		for (int j = i + 1; j < s.size(); j++) {
-			string a, b, c;
-			a = s.substr(0, i);
-			b = s.substr(i, j - i);
-			c = s.substr(j, s.size() - j + 1);
-			cout << a << " " << b << " " << c << endl;
-			if (a == b) cnt++;
-		}
+	int x = 1;
+	while (2 * x < s.size()) {
+		string a, b, c;
+		a = s.substr(0, x);
+		b = s.substr(x, x);
+		c = s.substr(2 * x, s.size() - 2 * x);
+		cout << a << " " << b << " " << c << endl;
+		if (a == b) cnt++;
+		x++;
 	}
 	cout << cnt;
 
